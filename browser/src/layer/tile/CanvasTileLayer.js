9dd0a0833996ac32bc15a9d2e6cae5257ef5b272
Chris Lord <chrislord.net@gmail.com>
2024-05-30 17:50:49 +0100

    Rename CanvasTileLayer.rawDeltas -> .compressedDeltas
    Signed-off-by: Chris Lord <chrislord.net@gmail.com>
    Change-Id: Idb46053e78fb44225f3f1b2a86b7ccaaf52310b2

--- browser/src/layer/tile/CanvasTileLayer.js
+++ browser/src/layer/tile/CanvasTileLayer.js
@@ -1206,7 +1206,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			current: true, // is this currently visible
 			canvas: null,  // canvas ready to render
 			imgDataCache: null, // flat byte array of canvas data
-			rawDeltas: null, // deltas ready to decompress
+			compressedDeltas: null, // deltas ready to decompress
 			deltaCount: 0, // how many deltas on top of the keyframe
 			updateCount: 0, // how many updates did we have
 			loadCount: 0, // how many times did we get a new keyframe
@@ -1224,7 +1224,7 @@ L.CanvasTileLayer = L.Layer.extend({
 				return this.invalidFrom >= this.wireId || !this.hasContent();
 			},
 			hasKeyframe: function() {
-				return this.rawDeltas && this.rawDeltas.length > 0;
+				return this.compressedDeltas && this.compressedDeltas.length > 0;
 			}
 		};
 		this._emptyTilesCount += 1;
@@ -5976,7 +5976,7 @@ L.CanvasTileLayer = L.Layer.extend({
 				if (this._debugDeltas)
 					window.app.console.log('Restoring a tile from cached delta at ' +
 							       this._tileCoordsToKey(tile.coords));
-				this._applyDelta(tile, tile.rawDeltas, true, false);
+				this._applyDelta(tile, tile.compressedDeltas, true, false);
 			}
 		}
 		if (!forPrefetch)
@@ -6031,7 +6031,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			var tile = this._tiles[keys[i]];
 			if (tile.canvas)
 				canvasKeys.push(keys[i]);
-			totalSize += tile.rawDeltas ? tile.rawDeltas.length : 0;
+			totalSize += tile.compressedDeltas ? tile.compressedDeltas.length : 0;
 		}
 
 		// Trim ourselves down to size.
@@ -6055,14 +6055,14 @@ L.CanvasTileLayer = L.Layer.extend({
 			{
 				var key = keys[i];
 				var tile = this._tiles[key];
-				if (tile.rawDeltas && !tile.current)
+				if (tile.compressedDeltas && !tile.current)
 				{
-					totalSize -= tile.rawDeltas.length;
+					totalSize -= tile.compressedDeltas.length;
 					if (this._debugDeltas)
 						window.app.console.log('Reclaim delta ' + key + ' memory: ' +
-								       tile.rawDeltas.length + ' bytes');
+								       tile.compressedDeltas.length + ' bytes');
 					this._reclaimTileCanvasMemory(tile);
-					tile.rawDeltas = null;
+					tile.compressedDeltas = null;
 					// force keyframe
 					tile.wireId = 0;
 					tile.invalidFrom = 0;
@@ -6155,33 +6155,33 @@ L.CanvasTileLayer = L.Layer.extend({
 		return resultu8;
 	},
 
-	_applyDelta: function(tile, rawDelta, isKeyframe, wireMessage) {
-		// 'Uint8Array' rawDelta
+	_applyDelta: function(tile, compressedDelta, isKeyframe, wireMessage) {
+		// 'Uint8Array' compressedDelta
 
 		if (this._debugDeltas)
-			window.app.console.log('Applying a raw ' + (isKeyframe ? 'keyframe' : 'delta') +
-					       ' of length ' + rawDelta.length +
-					       (this._debugDeltasDetail ? (' hex: ' + hex2string(rawDelta)) : ''));
+			window.app.console.log('Applying a compressed ' + (isKeyframe ? 'keyframe' : 'delta') +
+					       ' of length ' + compressedDelta.length +
+					       (this._debugDeltasDetail ? (' hex: ' + hex2string(compressedDelta)) : ''));
 
 		if (isKeyframe) {
 			// Important to do this before ensuring the context, or we'll needlessly
 			// reconstitute the old keyframe from compressed data.
-			if (tile.rawDeltas && tile.rawDeltas != rawDelta) { // help the gc?
-				tile.rawDeltas.length = 0;
-				tile.rawDeltas = null;
+			if (tile.compressedDeltas && tile.compressedDeltas != compressedDelta) { // help the gc?
+				tile.compressedDeltas.length = 0;
+				tile.compressedDeltas = null;
 				if (tile.imgDataCache)
 					tile.imgDataCache.length = 0;
 				tile.imgDataCache = null;
 			}
 		}
 
-		// Important to recurse & re-constitute from tile.rawDeelts
-		// before appending rawDelta and then applying it again.
+		// Important to recurse & re-constitute from tile.compressedDeltas
+		// before appending compressedDelta and then applying it again.
 		var ctx = this._ensureContext(tile);
 		if (!ctx) // out of canvas / texture memory.
 			return;
 
-		// if re-creating a canvas from rawDeltas don't update counts
+		// if re-creating a canvas from compressedDeltas don't update counts
 		if (wireMessage) {
 			if (isKeyframe) {
 				tile.loadCount++;
@@ -6190,7 +6190,7 @@ L.CanvasTileLayer = L.Layer.extend({
 				if (this._debug.tileDataOn) {
 					this._debug.tileDataAddLoad();
 				}
-			} else if (rawDelta.length === 0) {
+			} else if (compressedDelta.length === 0) {
 				tile.updateCount++;
 				this._nullDeltaUpdate++;
 				if (this._emptyDeltaDiv) {
@@ -6210,16 +6210,16 @@ L.CanvasTileLayer = L.Layer.extend({
 		// else - re-constituting from tile.rawData
 
 		var traceEvent = app.socket.createCompleteTraceEvent('L.CanvasTileLayer.applyDelta',
-								     { keyFrame: isKeyframe, length: rawDelta.length });
+								     { keyFrame: isKeyframe, length: compressedDelta.length });
 
 		// store the compressed version for later in its current
 		// form as byte arrays, so that we can manage our canvases
 		// better.
 		if (isKeyframe)
 		{
-			tile.rawDeltas = rawDelta; // overwrite
+			tile.compressedDeltas = compressedDelta; // overwrite
 		}
-		else if (!tile.rawDeltas)
+		else if (!tile.compressedDeltas)
 		{
 			window.app.console.log('Unusual: attempt to append a delta when we have no keyframe.');
 			return;
@@ -6227,10 +6227,10 @@ L.CanvasTileLayer = L.Layer.extend({
 		else // assume we already have a delta.
 		{
 			// FIXME: this is not beautiful; but no concatenate here.
-			var tmp = new Uint8Array(tile.rawDeltas.byteLength + rawDelta.byteLength);
-			tmp.set(tile.rawDeltas, 0);
-			tmp.set(rawDelta, tile.rawDeltas.byteLength);
-			tile.rawDeltas = tmp;
+			var tmp = new Uint8Array(tile.compressedDeltas.byteLength + compressedDelta.byteLength);
+			tmp.set(tile.compressedDeltas, 0);
+			tmp.set(compressedDelta, tile.compressedDeltas.byteLength);
+			tile.compressedDeltas = tmp;
 		}
 
 		// apply potentially several deltas in turn.
@@ -6238,7 +6238,7 @@ L.CanvasTileLayer = L.Layer.extend({
 		var offset = 0;
 
 		// FIXME:used clamped array ... as a 2nd parameter
-		var allDeltas = window.fzstd.decompress(rawDelta);
+		var allDeltas = window.fzstd.decompress(compressedDelta);
 
 		var imgData;
 
