CollaboraOnline/online

Move tile decompression and unpremultiplication into a Worker
Pre-allocate decompression buffers and use a Worker to decompress and
unpremultiply tile data asynchronously while avoiding memory allocation/
deallocation thrashing.

Signed-off-by: Chris Lord <chrislord.net@gmail.com>
Change-Id: I2282e3df8bf40bdc3a2381880d46df33f2451758

browser/src/core/SocketWorker.js
@@ -1,0 +1,112 @@
+/* -*- js-indent-level: 8; fill-column: 100 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/* eslint-disable no-inner-declarations */
+/* eslint no-unused-vars: ["warn", { "argsIgnorePattern": "^_" }] */
+/* global importScripts Uint8Array cool */
+
+if ('undefined' === typeof window) {
+	importScripts('../../node_modules/fzstd/umd/index.js');
+	importScripts('../layer/tile/CanvasTileUtils.js');
+	addEventListener('message', onMessage);
+
+	console.info('SocketWorker initialised', self.fzstd);
+
+	var keyframeBufferSize = 0;
+	var keyframeBuffer;
+
+	function onMessage(e) {
+		switch (e.data.message) {
+			case 'tile':
+				var buffer = e.data.buffer;
+				var offset = 0;
+				var processed = [];
+				var buffers = [buffer.buffer];
+				for (const tile of e.data.tiles) {
+					// Allocate key-frame decompression buffer
+					if (tile.size > keyframeBufferSize) {
+						keyframeBufferSize = tile.size;
+						keyframeBuffer = new Uint8Array(tile.size);
+					}
+
+					var startOffset = tile.isKeyframe ? 0 : offset;
+					var fzstdBuffer = tile.isKeyframe ? keyframeBuffer : buffer;
+					var fzstdOffset = startOffset;
+
+					var stream = new self.fzstd.Decompress((chunk, _isLast) => {
+						fzstdBuffer.set(chunk, fzstdOffset);
+						fzstdOffset += chunk.length;
+					});
+					stream.push(tile.rawData);
+
+					if (tile.isKeyframe) {
+						cool.CanvasTileUtils.unrle(
+							keyframeBuffer,
+							tile.tileSize,
+							tile.tileSize,
+							buffer,
+							offset,
+						);
+						startOffset = offset;
+						offset += tile.tileSize * tile.tileSize * 4;
+					} else offset = fzstdOffset;
+
+					var data = new Uint8Array(
+						buffer.buffer,
+						startOffset,
+						offset - startOffset,
+					);
+
+					// Unpremultiply delta updates
+					if (!tile.isKeyframe) {
+						var stop = false;
+						for (var i = 0; i < data.length && !stop; ) {
+							switch (data[i]) {
+								case 99: // 'c': // copy row
+									i += 4;
+									break;
+								case 100: // 'd': // new run
+									var span = data[i + 3] * 4;
+									i += 4;
+									cool.CanvasTileUtils.unpremultiply(data, span, i);
+									i += span;
+									break;
+								case 116: // 't': // terminate delta
+									stop = true;
+									i++;
+									break;
+								default:
+									console.error(
+										'[' + i + ']: ERROR: Unknown delta code ' + data[i],
+									);
+									i = data.length;
+									break;
+							}
+						}
+					}
+					processed.push({
+						id: tile.id,
+						rawData: tile.rawData,
+						processedData: data,
+					});
+					buffers.push(tile.rawData.buffer);
+				}
+				postMessage(
+					{ message: e.data.message, tiles: processed, buffer: buffer },
+					buffers,
+				);
+				break;
+
+			default:
+				console.error('Unrecognised preprocessor message', e);
+		}
+	}
+}

browser/src/layer/tile/CanvasTileLayer.js
@@ -3,7 +3,7 @@
  * L.CanvasTileLayer is a layer with canvas based rendering.
  */
 
-/* global app L JSDialog CanvasSectionContainer CanvasOverlay CDarkOverlay CSplitterLine $ _ CPointSet CPolyUtil CPolygon Cursor CCellSelection PathGroupType UNOKey UNOModifier Uint8ClampedArray Uint8Array Uint32Array */
+/* global app cool L JSDialog CanvasSectionContainer CanvasOverlay CDarkOverlay CSplitterLine $ _ CPointSet CPolyUtil CPolygon Cursor CCellSelection PathGroupType UNOKey UNOModifier Uint8ClampedArray Uint8Array */
 
 /*eslint no-extend-native:0*/
 if (typeof String.prototype.startsWith !== 'function') {
@@ -727,8 +727,6 @@ L.CanvasTileLayer = L.Layer.extend({
 		this._moveInProgress = false;
 		this._canonicalIdInitialized = false;
 		this._nullDeltaUpdate = 0;
-
-		this._unpremult = new L.UnPremult(256);
 	},
 
 	_initContainer: function () {
@@ -5364,7 +5362,7 @@ L.CanvasTileLayer = L.Layer.extend({
 				if (this._debugDeltas)
 					window.app.console.log('Restoring a tile from cached delta at ' +
 							       this._tileCoordsToKey(tile.coords));
-				this._applyDelta(tile, tile.rawDeltas, true, false);
+				this._applyDelta(tile, tile.rawDeltas, null, true, false);
 			}
 		}
 		if (!forPrefetch)
@@ -5520,8 +5518,8 @@ L.CanvasTileLayer = L.Layer.extend({
 		return ctx;
 	},
 
-	_applyDelta: function(tile, rawDelta, isKeyframe, wireMessage) {
-		// 'Uint8Array' rawDelta
+	_applyDelta: function(tile, rawDelta, decompressedDeltas, isKeyframe, wireMessage) {
+		// 'Uint8Array' rawDelta, decompressedDeltas
 
 		if (this._debugDeltas)
 			window.app.console.log('Applying a raw ' + (isKeyframe ? 'keyframe' : 'delta') +
@@ -5601,88 +5599,49 @@ L.CanvasTileLayer = L.Layer.extend({
 		// apply potentially several deltas in turn.
 		var i = 0;
 		var offset = 0;
-
-		// FIXME:used clamped array ... as a 2nd parameter
-		var allDeltas = window.fzstd.decompress(rawDelta);
-
+		var needsUnpremultiply = false;
 		var imgData;
 
 		// May have been changed by _ensureContext garbage collection
 		var canvas = tile.canvas;
 
-		if (isKeyframe)
-		{
-			if (this._debugDeltas)
-				window.app.console.log('Applying a raw RLE keyframe of length ' + allDeltas.length +
-						       ' hex: ' + hex2string(allDeltas, allDeltas.length));;
+		if (!decompressedDeltas) {
+			// FIXME:used clamped array ... as a 2nd parameter
+			decompressedDeltas = window.fzstd.decompress(rawDelta);
 
-			// Byte bashing fun
-			var width = canvas.width;
-			var height = canvas.height;
-
-			var resultu32 = new Uint32Array(width * height);
-			var resultu8 = new Uint8ClampedArray(resultu32.buffer, resultu32.byteOffset, resultu32.byteLength);
-
-			for (var y = 0; y < height; ++y)
-			{
-				var rleSize = allDeltas[offset] + allDeltas[offset+1] * 256;
-				offset += 2;
+			if (isKeyframe) {
 				if (this._debugDeltas)
-					window.app.console.log('rle size ' + rleSize);
+					window.app.console.log('Applying a raw RLE keyframe of length ' + decompressedDeltas.length +
+								   ' hex: ' + hex2string(decompressedDeltas, decompressedDeltas.length));;
 
-				var rleMask = offset;
-				var rleMaskSizeBytes = 256/8;
+				var width = canvas.width;
+				var height = canvas.height;
 
-				offset += rleMaskSizeBytes;
+				var result = new Uint8Array(width * height * 4);
+				offset = cool.CanvasTileUtils.unrle(decompressedDeltas, width, height, result);
 
-				var uniquePixels;
-				if (rleSize > 0)
-				{
-					this._unpremult.unpremultiply(allDeltas, rleSize * 4, offset);
-					uniquePixels = this._unpremult._pixels;
-					if (this._debugDeltas)
-						window.app.console.log(
-							'Pixels hex: ' + hex2string(this._unpremult._bytes, rleSize * 4));;
-				}
+				imgData = new ImageData(new Uint8ClampedArray(result.buffer), width, height);
 
-				// It would be rather nice to have real 64bit types [!]
-				var lastPix = 0;
-				var lastMask = 0;
-				var bitToCheck = 256;
-				var rleMaskOffset = rleMask;
-
-				var pixOffset = y * width;
-				var pixSrc = 0;
-
-				for (var x = 0; x < width; ++x)
-				{
-					if (bitToCheck > 128)
-					{
-						bitToCheck = 1;
-						lastMask = allDeltas[rleMaskOffset++];
-					}
-					if (!(lastMask & bitToCheck))
-						lastPix = uniquePixels[pixSrc++];
-					bitToCheck = bitToCheck << 1;
-					resultu32[pixOffset++] = lastPix;
-				}
-
-				offset += rleSize * 4;
+				if (this._debugDeltas)
+					window.app.console.log('Applied keyframe of total size ' + offset +
+								   ' at stream offset 0');
 			}
 
-			imgData = new ImageData(resultu8, canvas.width, canvas.height);
-
-			if (this._debugDeltas)
-				window.app.console.log('Applied keyframe of total size ' + offset +
-						       ' at stream offset 0');
+			// delta updates will need to be unpremultiplied
+			needsUnpremultiply = true;
+		} else if (isKeyframe) {
+			var width = canvas.width;
+			var height = canvas.height;
+			imgData = new ImageData(new Uint8ClampedArray(decompressedDeltas.buffer, decompressedDeltas.byteOffset, decompressedDeltas.byteLength), width, height);
+			offset = width * height * 4;
 		}
 
-		while (offset < allDeltas.length)
+		while (offset < decompressedDeltas.length)
 		{
 			if (this._debugDeltas)
-				window.app.console.log('Next delta at ' + offset + ' length ' + (allDeltas.length - offset));
+				window.app.console.log('Next delta at ' + offset + ' length ' + (decompressedDeltas.length - offset));
 
-			var delta = !offset ? allDeltas : allDeltas.subarray(offset);
+			var delta = !offset ? decompressedDeltas : decompressedDeltas.subarray(offset);
 
 			// Debugging paranoia: if we get this wrong bad things happen.
 			if (delta.length >= canvas.width * canvas.height * 4)
@@ -5703,7 +5662,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			// copy old data to work from:
 			var oldData = new Uint8ClampedArray(imgData.data);
 
-			var len = this._applyDeltaChunk(imgData, delta, oldData, canvas.width, canvas.height);
+			var len = this._applyDeltaChunk(imgData, delta, oldData, canvas.width, canvas.height, needsUnpremultiply);
 			if (this._debugDeltas)
 				window.app.console.log('Applied chunk ' + i++ + ' of total size ' + delta.length +
 						       ' at stream offset ' + offset + ' size ' + len);
@@ -5722,7 +5681,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			traceEvent.finish();
 	},
 
-	_applyDeltaChunk: function(imgData, delta, oldData, width, height) {
+	_applyDeltaChunk: function(imgData, delta, oldData, width, height, needsUnpremultiply) {
 		var pixSize = width * height * 4;
 		if (this._debugDeltas)
 			window.app.console.log('Applying a delta of length ' +
@@ -5778,11 +5737,11 @@ L.CanvasTileLayer = L.Layer.extend({
 							       ' at pos ' + destCol + ', ' + destRow + ' into delta at byte: ' + offset);
 				i += 4;
 				span *= 4;
-				// copy so this is suitably aligned for a Uint32Array view
-				this._unpremult.unpremultiply(delta, span, i);
+				if (needsUnpremultiply)
+					cool.CanvasTileUtils.unpremultiply(delta, span, i);
 				// imgData.data[offset + 1] = 256; // debug - greener start
-				for (var j = 0; j < span; ++j)
-					imgData.data[offset++] = this._unpremult._bytes[j];
+				imgData.data.set(delta.subarray(i, i + span), offset);
+				offset += span;
 				i += span;
 				// imgData.data[offset - 2] = 256; // debug - blue terminator
 				break;
@@ -5791,7 +5750,7 @@ L.CanvasTileLayer = L.Layer.extend({
 				i++;
 				break;
 			default:
-				console.log('[' + i + ']: ERROR: Unknown delta code ' + delta[i]);
+				console.error('[' + i + ']: ERROR: Unknown delta code ' + delta[i]);
 				i = delta.length;
 				break;
 			}
@@ -5874,7 +5833,7 @@ L.CanvasTileLayer = L.Layer.extend({
 		// updates don't need more chattiness with a tileprocessed
 		if (hasContent)
 		{
-			this._applyDelta(tile, img.rawData, img.isKeyframe, true);
+			this._applyDelta(tile, img.rawData, img.processedData, img.isKeyframe, true);
 			this._tileReady(coords);
 		}
 

caolanm/cool

browser/src/core/SocketWorker.js
@@ -1,0 +1,114 @@
+/* -*- js-indent-level: 8; fill-column: 100 -*- */
+/*
+ * Copyright the Collabora Online contributors.
+ *
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/* eslint-disable no-inner-declarations */
+/* eslint no-unused-vars: ["warn", { "argsIgnorePattern": "^_" }] */
+/* global importScripts Uint8Array */
+
+if ('undefined' === typeof window) {
+	self.L = {};
+
+	importScripts('../../node_modules/fzstd/umd/index.js');
+	importScripts('../layer/tile/CanvasTileUtils.js');
+	addEventListener('message', onMessage);
+
+	console.info('SocketWorker initialised');
+
+	var keyframeBufferSize = 0;
+	var keyframeBuffer;
+
+	function onMessage(e) {
+		switch (e.data.message) {
+			case 'tile':
+				var buffer = e.data.buffer;
+				var offset = 0;
+				var processed = [];
+				var buffers = [buffer.buffer];
+				for (const tile of e.data.tiles) {
+					// Allocate key-frame decompression buffer
+					if (tile.size > keyframeBufferSize) {
+						keyframeBufferSize = tile.size;
+						keyframeBuffer = new Uint8Array(tile.size);
+					}
+
+					var startOffset = tile.isKeyframe ? 0 : offset;
+					var fzstdBuffer = tile.isKeyframe ? keyframeBuffer : buffer;
+					var fzstdOffset = startOffset;
+
+					var stream = new self.fzstd.Decompress((chunk, _isLast) => {
+						fzstdBuffer.set(chunk, fzstdOffset);
+						fzstdOffset += chunk.length;
+					});
+					stream.push(tile.rawData);
+
+					if (tile.isKeyframe) {
+						L.CanvasTileUtils.unrle(
+							keyframeBuffer,
+							tile.tileSize,
+							tile.tileSize,
+							buffer,
+							offset,
+						);
+						startOffset = offset;
+						offset += tile.tileSize * tile.tileSize * 4;
+					} else offset = fzstdOffset;
+
+					var data = new Uint8Array(
+						buffer.buffer,
+						startOffset,
+						offset - startOffset,
+					);
+
+					// Unpremultiply delta updates
+					if (!tile.isKeyframe) {
+						var stop = false;
+						for (var i = 0; i < data.length && !stop; ) {
+							switch (data[i]) {
+								case 99: // 'c': // copy row
+									i += 4;
+									break;
+								case 100: // 'd': // new run
+									var span = data[i + 3] * 4;
+									i += 4;
+									L.CanvasTileUtils.unpremultiply(data, span, i);
+									i += span;
+									break;
+								case 116: // 't': // terminate delta
+									stop = true;
+									i++;
+									break;
+								default:
+									console.error(
+										'[' + i + ']: ERROR: Unknown delta code ' + data[i],
+									);
+									i = data.length;
+									break;
+							}
+						}
+					}
+					processed.push({
+						id: tile.id,
+						rawData: tile.rawData,
+						processedData: data,
+					});
+					buffers.push(tile.rawData.buffer);
+				}
+				postMessage(
+					{ message: e.data.message, tiles: processed, buffer: buffer },
+					buffers,
+				);
+				break;
+
+			default:
+				console.error('Unrecognised preprocessor message', e);
+		}
+	}
+}

browser/src/layer/tile/CanvasTileLayer.js
@@ -3,7 +3,7 @@
  * L.CanvasTileLayer is a layer with canvas based rendering.
  */
 
-/* global app L JSDialog CanvasSectionContainer GraphicSelection CanvasOverlay CDarkOverlay CSplitterLine CursorHeaderSection $ _ CPointSet CPolyUtil CPolygon Cursor CCellSelection PathGroupType UNOKey UNOModifier Uint8ClampedArray Uint8Array Uint32Array */
+/* global app L JSDialog CanvasSectionContainer GraphicSelection CanvasOverlay CDarkOverlay CSplitterLine CursorHeaderSection $ _ CPointSet CPolyUtil CPolygon Cursor CCellSelection PathGroupType UNOKey UNOModifier Uint8ClampedArray Uint8Array */
 
 /*eslint no-extend-native:0*/
 if (typeof String.prototype.startsWith !== 'function') {
@@ -725,8 +725,6 @@ L.CanvasTileLayer = L.Layer.extend({
 		this._moveTileRequests = [];
 		this._canonicalIdInitialized = false;
 		this._nullDeltaUpdate = 0;
-
-		this._unpremult = new L.UnPremult(256);
 	},
 
 	_initContainer: function () {
@@ -5120,7 +5118,7 @@ L.CanvasTileLayer = L.Layer.extend({
 				if (this._debugDeltas)
 					window.app.console.log('Restoring a tile from cached delta at ' +
 							       this._tileCoordsToKey(tile.coords));
-				this._applyDelta(tile, tile.rawDeltas, true, false);
+				this._applyDelta(tile, tile.rawDeltas, null, true, false);
 			}
 		}
 		if (!forPrefetch)
@@ -5276,8 +5274,8 @@ L.CanvasTileLayer = L.Layer.extend({
 		return ctx;
 	},
 
-	_applyDelta: function(tile, rawDelta, isKeyframe, wireMessage) {
-		// 'Uint8Array' rawDelta
+	_applyDelta: function(tile, rawDelta, decompressedDeltas, isKeyframe, wireMessage) {
+		// 'Uint8Array' rawDelta, decompressedDeltas
 
 		if (this._debugDeltas)
 			window.app.console.log('Applying a raw ' + (isKeyframe ? 'keyframe' : 'delta') +
@@ -5357,88 +5355,49 @@ L.CanvasTileLayer = L.Layer.extend({
 		// apply potentially several deltas in turn.
 		var i = 0;
 		var offset = 0;
-
-		// FIXME:used clamped array ... as a 2nd parameter
-		var allDeltas = window.fzstd.decompress(rawDelta);
-
+		var needsUnpremultiply = false;
 		var imgData;
 
 		// May have been changed by _ensureContext garbage collection
 		var canvas = tile.canvas;
 
-		if (isKeyframe)
-		{
-			if (this._debugDeltas)
-				window.app.console.log('Applying a raw RLE keyframe of length ' + allDeltas.length +
-						       ' hex: ' + hex2string(allDeltas, allDeltas.length));;
+		if (!decompressedDeltas) {
+			// FIXME:used clamped array ... as a 2nd parameter
+			decompressedDeltas = window.fzstd.decompress(rawDelta);
 
-			// Byte bashing fun
-			var width = canvas.width;
-			var height = canvas.height;
-
-			var resultu32 = new Uint32Array(width * height);
-			var resultu8 = new Uint8ClampedArray(resultu32.buffer, resultu32.byteOffset, resultu32.byteLength);
-
-			for (var y = 0; y < height; ++y)
-			{
-				var rleSize = allDeltas[offset] + allDeltas[offset+1] * 256;
-				offset += 2;
+			if (isKeyframe) {
 				if (this._debugDeltas)
-					window.app.console.log('rle size ' + rleSize);
-
-				var rleMask = offset;
-				var rleMaskSizeBytes = 256/8;
-
-				offset += rleMaskSizeBytes;
-
-				var uniquePixels;
-				if (rleSize > 0)
-				{
-					this._unpremult.unpremultiply(allDeltas, rleSize * 4, offset);
-					uniquePixels = this._unpremult._pixels;
-					if (this._debugDeltas)
-						window.app.console.log(
-							'Pixels hex: ' + hex2string(this._unpremult._bytes, rleSize * 4));;
-				}
-
-				// It would be rather nice to have real 64bit types [!]
-				var lastPix = 0;
-				var lastMask = 0;
-				var bitToCheck = 256;
-				var rleMaskOffset = rleMask;
-
-				var pixOffset = y * width;
-				var pixSrc = 0;
-
-				for (var x = 0; x < width; ++x)
-				{
-					if (bitToCheck > 128)
-					{
-						bitToCheck = 1;
-						lastMask = allDeltas[rleMaskOffset++];
-					}
-					if (!(lastMask & bitToCheck))
-						lastPix = uniquePixels[pixSrc++];
-					bitToCheck = bitToCheck << 1;
-					resultu32[pixOffset++] = lastPix;
-				}
-
-				offset += rleSize * 4;
+					window.app.console.log('Applying a raw RLE keyframe of length ' + decompressedDeltas.length +
+								   ' hex: ' + hex2string(decompressedDeltas, decompressedDeltas.length));;
+	
+				var width = canvas.width;
+				var height = canvas.height;
+	
+				var result = new Uint8Array(width * height * 4);
+				offset = L.CanvasTileUtils.unrle(decompressedDeltas, width, height, result);
+	
+				imgData = new ImageData(new Uint8ClampedArray(result.buffer), width, height);
+	
+				if (this._debugDeltas)
+					window.app.console.log('Applied keyframe of total size ' + offset +
+								   ' at stream offset 0');
 			}
 
-			imgData = new ImageData(resultu8, canvas.width, canvas.height);
-
-			if (this._debugDeltas)
-				window.app.console.log('Applied keyframe of total size ' + offset +
-						       ' at stream offset 0');
+			// delta updates will need to be unpremultiplied
+			needsUnpremultiply = true;
+		} else if (isKeyframe) {
+			var width = canvas.width;
+			var height = canvas.height;
+			imgData = new ImageData(new Uint8ClampedArray(decompressedDeltas.buffer, decompressedDeltas.byteOffset, decompressedDeltas.byteLength), width, height);
+			offset = width * height * 4;
 		}
 
-		while (offset < allDeltas.length)
+		while (offset < decompressedDeltas.length)
 		{
 			if (this._debugDeltas)
-				window.app.console.log('Next delta at ' + offset + ' length ' + (allDeltas.length - offset));
+				window.app.console.log('Next delta at ' + offset + ' length ' + (decompressedDeltas.length - offset));
 
-			var delta = !offset ? allDeltas : allDeltas.subarray(offset);
+			var delta = !offset ? decompressedDeltas : decompressedDeltas.subarray(offset);
 
 			// Debugging paranoia: if we get this wrong bad things happen.
 			if (delta.length >= canvas.width * canvas.height * 4)
@@ -5459,7 +5418,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			// copy old data to work from:
 			var oldData = new Uint8ClampedArray(imgData.data);
 
-			var len = this._applyDeltaChunk(imgData, delta, oldData, canvas.width, canvas.height);
+			var len = this._applyDeltaChunk(imgData, delta, oldData, canvas.width, canvas.height, needsUnpremultiply);
 			if (this._debugDeltas)
 				window.app.console.log('Applied chunk ' + i++ + ' of total size ' + delta.length +
 						       ' at stream offset ' + offset + ' size ' + len);
@@ -5478,7 +5437,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			traceEvent.finish();
 	},
 
-	_applyDeltaChunk: function(imgData, delta, oldData, width, height) {
+	_applyDeltaChunk: function(imgData, delta, oldData, width, height, needsUnpremultiply) {
 		var pixSize = width * height * 4;
 		if (this._debugDeltas)
 			window.app.console.log('Applying a delta of length ' +
@@ -5534,11 +5493,11 @@ L.CanvasTileLayer = L.Layer.extend({
 							       ' at pos ' + destCol + ', ' + destRow + ' into delta at byte: ' + offset);
 				i += 4;
 				span *= 4;
-				// copy so this is suitably aligned for a Uint32Array view
-				this._unpremult.unpremultiply(delta, span, i);
+				if (needsUnpremultiply)
+					L.CanvasTileUtils.unpremultiply(delta, span, i);
 				// imgData.data[offset + 1] = 256; // debug - greener start
-				for (var j = 0; j < span; ++j)
-					imgData.data[offset++] = this._unpremult._bytes[j];
+				imgData.data.set(delta.subarray(i, i + span), offset);
+				offset += span;
 				i += span;
 				// imgData.data[offset - 2] = 256; // debug - blue terminator
 				break;
@@ -5547,7 +5506,7 @@ L.CanvasTileLayer = L.Layer.extend({
 				i++;
 				break;
 			default:
-				console.log('[' + i + ']: ERROR: Unknown delta code ' + delta[i]);
+				console.error('[' + i + ']: ERROR: Unknown delta code ' + delta[i]);
 				i = delta.length;
 				break;
 			}
@@ -5630,7 +5589,7 @@ L.CanvasTileLayer = L.Layer.extend({
 		// updates don't need more chattiness with a tileprocessed
 		if (hasContent)
 		{
-			this._applyDelta(tile, img.rawData, img.isKeyframe, true);
+			this._applyDelta(tile, img.rawData, img.processedData, img.isKeyframe, true);
 			this._tileReady(coords);
 		}
 